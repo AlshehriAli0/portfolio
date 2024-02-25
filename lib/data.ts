28adce4f087e0bc96569051b6695dbc72dca1117
Ali Alshehri <Ali0fawzish@outlook.com>
2024-02-25 14:07:25 +0300

    Remove unused imports and update image paths

--- lib/data.ts
+++ lib/data.ts
@@ -1,7 +1,5 @@
 import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
+
 import mindboard from "/public/mindboard.png";
 import analasis from "@/public/tech-data.png";
 import todo from "@/public/todo.png";

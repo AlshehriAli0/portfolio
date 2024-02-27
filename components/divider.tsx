48b43a57b08c96191246d4d4c215d343d9b25cab
Ali Alshehri <Ali0fawzish@outlook.com>
2024-02-27 14:39:35 +0300

    commit

--- components/divider.tsx
+++ components/divider.tsx
@@ -6,7 +6,7 @@ import { motion, m } from "framer-motion";
 export default function Divider() {
   return (
     <m.div
-      className="bg-slate-200 sm:my-24 my-16 h-16 w-1 rounded-full dark:bg-opacity-20 motion-safe"
+      className="bg-slate-200 sm:my-24 my-16 h-16 w-1 rounded-full dark:bg-opacity-20 "
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 1 }}
       transition={{ delay: 0.17 }}

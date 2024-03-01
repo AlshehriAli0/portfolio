d90e640335fdd7eed5865916df883ccbf0ad5590
Ali Alshehri <Ali0fawzish@outlook.com>
2024-03-01 16:17:35 +0300

    Fix dynamic import of ScrambleText component and remove unnecessary text in Theme button

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import React from "react";
-import ScrambleText from "./scramble-text";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 import IntroButtons from "./intro-buttons";
+import dynamic from "next/dynamic";
+
+const ScrambleText = dynamic(() => import("@/components/scramble-text"));
 
 export default function Intro() {
   const { ref } = useSectionInView({ sectionName: "Home", threshold: 0.4 });

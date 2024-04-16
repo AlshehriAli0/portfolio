daf88e42ae6682a57d6132a18651e327fa8b6624
Ali Alshehri <Ali0fawzish@outlook.com>
2024-04-16 21:52:22 +0300

    Update resume link and remove PDF file

--- components/intro-buttons.tsx
+++ components/intro-buttons.tsx
@@ -36,7 +36,7 @@ export default function IntroButtons() {
       <Link
         prefetch={false}
         className="group bg-white w-50 justify-center px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-105 active:scale-100 transition cursor-pointer border border-black/10 dark:bg-white/10 dark:hover:bg-white/[0.17]"
-        href="https://utfs.io/f/844c5473-2a8d-4a72-b807-640dbab11360-7jsb4d.pdf"
+        href="https://utfs.io/f/9b1c3bc3-b147-4312-bf40-148648770dbf-7jsb4d.pdf"
         target="_blank"
         rel="noopener noreferrer"
       >
@@ -47,7 +47,7 @@ export default function IntroButtons() {
         <Link
           prefetch={false}
           className="bg-white p-4 text-slate-700 dark:text-white/90 dark:bg-white/10 px-[1.1405rem] rounded-full flex items-center gap-2  focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10 dark:hover:bg-white/[0.17]"
-          href="https://utfs.io/f/9b1c3bc3-b147-4312-bf40-148648770dbf-7jsb4d.pdf"
+          href="https://www.linkedin.com/in/ali-alshehri-340b26284"
           target="_blank"
           aria-label="linkedin"
           rel="noopener noreferrer"
